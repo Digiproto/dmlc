456d837925559d746aa21f134f8bd9e557e39ba9
alloc <alloc.young@gmail.com>
2013-07-15 10:40:02 +0800

    Add object method generation
    Add object method generation
    
    Signed-off-by: alloc <alloc.young@gmail.com>
    Signed-off-by: shenoubang <shenoubang@gmail.com>
    Signed-off-by: eJim Lee <lyjforjob@gmail.com>

---
+++ gen_object.c
@@ -0,0 +1,87 @@
+/*
+ * gen_object.c: 
+ *
+ * Copyright (C) 2013 alloc <alloc.young@gmail.com>
+ * Skyeye Develop Group, for help please send mail to
+ * <skyeye-developer@lists.gro.clinux.org>
+ *
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+/**
+ * @file gen_object.c
+ * @brief object code framework
+ * @author alloc, alloc.young@gmail.com
+ * @version 1.0
+ * @date 2013-05-24
+ */
+
+#include "gen_object.h"
+extern int method_to_generate;
+
+static void gen_object_method(object_t *obj){
+	struct method_name *m;
+	struct list_head *p;
+
+	list_for_each(p,&obj->methods){
+		m = list_entry(p,struct method_name, entry);
+		gen_dml_method(obj, m);
+	}	
+	add_object_generated_method(obj);	
+} 
+
+static void gen_obj_generic_code(object_t *obj){
+	struct list_head *p;
+	object_t *t;
+
+	gen_object_method(obj);
+
+	list_for_each(p, &obj->childs){
+		t = list_entry(p, object_t, entry);
+		gen_obj_generic_code(t);
+	}
+}
+
+static void gen_device_code(object_t *obj){
+	struct list_head *p;
+	object_t *t;
+	device_t *dev = (device_t *)obj;
+
+	gen_obj_generic_code(obj);
+	/*
+	list_for_each(p,&dev->implements){
+		t = list_entry(p,object_t,entry);
+		gen_obj_generic_code(t);
+	} */
+	/*connect,port ect*/
+}
+
+extern FILE *out;
+
+void gen_dml_code(device_t *dev, FILE* f) {
+	int round = 0;
+
+	out = f;
+	gen_code_once_noplatform(dev, f); 
+
+	
+	while(method_to_generate){
+		gen_device_code(&dev->obj);
+		round++;
+		printf("code generation round %d, method to generate %d\n",round, method_to_generate);
+	}
+}
+
+
---
+++ gen_object.h
@@ -0,0 +1,33 @@
+/*
+ * gen_object.h: 
+ *
+ * Copyright (C) 2013 alloc <alloc.young@gmail.com>
+ * Skyeye Develop Group, for help please send mail to
+ * <skyeye-developer@lists.gro.clinux.org>
+ *
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef __GEN_OBJECT_H__ 
+#define __GEN_OBJECT_H__  
+
+#include "object.h"
+#include "gen_once_noplatform.h"
+#include "gen_common.h"
+
+void gen_dml_code(device_t *dev, FILE *f);
+
+#endif /* __GEN_OBJECT_H__ */
