2b65a3c08142539f53f8c543a900642a796cfb6a
alloc <alloc.young@gmail.com>
2013-07-18 10:08:02 +0800

    Add skyeye platform code
    Add skyeye platform code
    
    Signed-off-by: alloc <alloc.young@gmail.com>
    Signed-off-by: shenoubang <shenoubang@gmail.com>
    Signed-off-by: eJim Lee <lyjforjob@gmail.com>

---
+++ skyeye_platform.h
@@ -0,0 +1,32 @@
+/*
+ * skyeye_platform.h: 
+ *
+ * Copyright (C) 2013 alloc <alloc.young@gmail.com>
+ * Skyeye Develop Group, for help please send mail to
+ * <skyeye-developer@lists.gro.clinux.org>
+ *
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef __SKYEYE_PLATFORM_H__ 
+#define __SKYEYE_PLATFORM_H__  
+#include "object.h"
+#include "gen_object.h"
+
+void gen_skyeye_cfile(device_t *dev, const char *out_dir);
+
+
+#endif /* __SKYEYE_PLATFORM_H__ */
