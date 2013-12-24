37c621bb106a0e37369e6e7df39b5890f06a3953
alloc <alloc.young@gmail.com>
2013-12-24 16:55:28 +0800

    Change connect and set attr function protype
    Change connect and set attr function protype
    
    Signed-off-by: alloc <alloc.young@gmail.com>
    Signed-off-by: shenoubang <shenobuang@gmail.com>
    Signed-off-by: shenoubang <shenoubang@gmail.com>

--- lib/dml_common/simics/core/object_iface.h
+++ lib/dml_common/simics/core/object_iface.h
@@ -25,6 +25,7 @@
 #define __OBJECT_IFACE_H__  
 #include <stdlib.h>
 #include "../base_types.h"
+#include <simics/core/attribute.h>
 struct InterfaceDescription {
     const char *name;
     const void *iface;
@@ -35,18 +36,14 @@ struct PortDescription {
     const struct InterfaceDescription *id;
 };
 
-typedef int (*connect_set_t)(struct conf_object *dev1, struct conf_object *dev2, const char *port, int idx);
-typedef int (*connect_get_t)(struct conf_object *dev1, struct conf_object **dev2, char **port,int idx);
+typedef int          (*sim_set_attr)(void *_, struct conf_object *obj, attr_value_t *attr, attr_value_t *index);
+typedef attr_value_t (*sim_get_attr)(void *_, struct conf_object *obj, attr_value_t *index);
 
 struct ConnectDescription {
     const char *name;
-    connect_set_t set;
-    connect_get_t get;
+    sim_set_t set;
+    sim_get_t get;
 };
-
-typedef void* attr_value_t;
-typedef int          (*sim_set_attr)(struct conf_object *obj, attr_value_t attr);
-typedef attr_value_t (*sim_get_attr)(struct conf_object *obj);
 
 struct AttributeDescription {
 	const char *name;
@@ -58,5 +55,5 @@ const void *SIM_get_interface(conf_object_t *dev, const char *iface);
 const void *SIM_get_port_interface(conf_object_t *dev, const char *iface, const char *port);
 int SIM_obj_connect(conf_object_t *dev1, const char *name, conf_object_t *dev2);
 int SIM_connect_port(conf_object_t *dev1, const char *name, conf_object_t *dev2, const char *port);
-int SIM_set_attr(conf_object_t *dev, const char *name, attr_value_t attr);
+int SIM_set_attr(conf_object_t *dev, const char *name, attr_value_t *attr, attr_value_t *index);
 #endif /* __OBJECT_IFACE_H__ */
