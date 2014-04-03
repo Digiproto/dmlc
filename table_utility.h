775ea5719d9af6587721332e2c4cdbf5cdc8046e
alloc <alloc.young@gmail.com>
2014-04-03 19:41:54 +0800

    Add table visitor function
    Add table visitor function
    
    Signed-off-by: alloc <alloc.young@gmail.com>
    Signed-off-by: shenoubang <shenoubang@gmail.com>

---
+++ table_utility.h
@@ -0,0 +1,30 @@
+/*
+ * table_utility.h: 
+ *
+ * Copyright (C) 2013 alloc <alloc.young@gmail.com>
+ * Skyeye Develop Group, for help please send mail to
+ * <skyeye-developer@lists.gro.clinux.org>
+ *
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef __TABLE_UTILITY_H__
+#define __TABLE_UTILITY_H__  
+#include "object.h"
+typedef void (*visitor)(symbol_t sym, void *data);
+void table_visit(symtab_t table, type_t type, visitor cb, void *data);
+
+#endif /* __TABLE_UTILITY_H__ */
