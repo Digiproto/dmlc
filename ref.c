3c34f26449df43815720de28373709a8f5eb0d64
alloc <alloc.young@gmail.com>
2014-04-14 17:08:59 +0800

    Reindention ref.c
    Reindention ref.c
    
    Signed-off-by: alloc <alloc.young@gmail.com>
    Signed-off-by: shenoubang <shenoubang@gmail.com>

--- ref.c
+++ ref.c
@@ -57,9 +57,9 @@ node_info_t  *new_node_info(tree_t *node, tree_t *index) {
  * @return : pointer to reference information
  */
 ref_info_t *new_ref_info(void) {
-        ref_info_t *ref = (ref_info_t *)gdml_zmalloc(sizeof(*ref));
-        ref_info_init(ref);
-        return ref;
+	ref_info_t *ref = (ref_info_t *)gdml_zmalloc(sizeof(*ref));
+	ref_info_init(ref);
+	return ref;
 }
 
 /**
@@ -111,7 +111,6 @@ void ref_info_print(ref_info_t *fi) {
 		node = ni->node;
 		my_DBG("name %s\n", node->common.name);
 	}
-
 }
 
 /**
@@ -121,37 +120,38 @@ void ref_info_print(ref_info_t *fi) {
  * @param fi : the list of reference
  */
 void collect_ref_info(tree_t *expr, ref_info_t *fi){
-        tree_t *node;
-        node_info_t *ni;
-        type_t type = expr->common.type;
-        if(type == IDENT_TYPE){
+	tree_t *node;
+	node_info_t *ni;
+	type_t type = expr->common.type;
+
+	if(type == IDENT_TYPE){
 		ni = new_node_info(expr, NULL);
-                add_node_info(fi, ni);
-                return;
-        }else if(type == QUOTE_TYPE){
-                my_DBG("quote type\n");
+		add_node_info(fi, ni);
+		return;
+	}else if(type == QUOTE_TYPE){
+		my_DBG("quote type\n");
 		ni = new_node_info(expr, NULL);
-                add_node_info(fi, ni);
-                return;
-        }else if(type == COMPONENT_TYPE){
-                node = expr->component.expr;
-                my_DBG("expr %p \n", node);
-                collect_ref_info(node,fi);
+		add_node_info(fi, ni);
+		return;
+	}else if(type == COMPONENT_TYPE){
+		node = expr->component.expr;
+		my_DBG("expr %p \n", node);
+		collect_ref_info(node,fi);
 		node = dml_keyword_node(expr->component.comp, &expr->common.location);
 		ni = new_node_info(node, NULL);
-                add_node_info(fi,ni);
-                node = expr->component.ident;
-                my_DBG("ident %s, node %p\n", node->ident.str, node);
+		add_node_info(fi,ni);
+		node = expr->component.ident;
+		my_DBG("ident %s, node %p\n", node->ident.str, node);
 		ni = new_node_info(node, NULL);
-                add_node_info(fi,ni);
-        } else if (type == DML_KEYWORD_TYPE) {
-                /*bank field */
+		add_node_info(fi,ni);
+	} else if (type == DML_KEYWORD_TYPE) {
+		/*bank field */
 		ni = new_node_info(expr, NULL);
-                add_node_info(fi, ni);
-        }  else if (type == BIT_SLIC_EXPR_TYPE) {
-			collect_ref_info(expr->bit_slic.expr, fi);
-			ni = new_node_info(NULL, expr->bit_slic.bit);
-			add_node_info(fi, ni);
+		add_node_info(fi, ni);
+	}  else if (type == BIT_SLIC_EXPR_TYPE) {
+		collect_ref_info(expr->bit_slic.expr, fi);
+		ni = new_node_info(NULL, expr->bit_slic.bit);
+		add_node_info(fi, ni);
 	} else {
 		my_DBG("TODO: other type %d\n", type);
 	}
@@ -178,7 +178,7 @@ void printf_ref(ref_ret_t *ref_ret){
 	object_t *con = NULL;
 
 	if(ref_ret->con) {
-			con = ref_ret->con;
+		con = ref_ret->con;
 	}
 
 	fi = ref_ret->ref;
@@ -187,63 +187,63 @@ void printf_ref(ref_ret_t *ref_ret){
 	while(p != head) {
 		ni = list_entry(p, node_info_t, entry);
 		node = ni->node;
-		normal_case:
+	normal_case:
 		if(node) {
 			if(node->common.type == IDENT_TYPE){
-					name = node->ident.str;
+				name = node->ident.str;
 			} else if (node->common.type == DML_KEYWORD_TYPE){
-					//name = node->common.name;
-					name = node->ident.str;
+				//name = node->common.name;
+				name = node->ident.str;
 			}else if(node->common.type == QUOTE_TYPE){
-					tmp = node->quote.ident;
-					name = tmp->ident.str;
+				tmp = node->quote.ident;
+				name = tmp->ident.str;
 			} else {
-					my_DBG("node name %s type %d\n",node->common.name,node->common.type);
+				my_DBG("node name %s type %d\n",node->common.name,node->common.type);
 
 			}
 			if(i == 0){
-					i++;
-					sym = symbol_find_notype(current_table, name);
-					if(!sym){
-							my_DBG("no sym %s found in current symtab \n",name);
+				i++;
+				sym = symbol_find_notype(current_table, name);
+				if(!sym){
+					my_DBG("no sym %s found in current symtab \n",name);
+				}
+				if(ref_ret->is_obj) {
+					obj = (object_t *)sym->attr;
+					alias = get_obj_ref(obj);
+					if(obj->is_array) {
+						int len = 0;
+						tmp_str = strrchr(alias, '[');	
+						if(tmp_str) {
+							len = tmp_str - alias;
+						}
+						tmp_str = gdml_zmalloc(len + 1);
+						memcpy(tmp_str, alias, len);
+						tmp_str[len] = '\0';	
+						alias = tmp_str;
 					}
-					if(ref_ret->is_obj) {
-							obj = (object_t *)sym->attr;
-							alias = get_obj_ref(obj);
-							if(obj->is_array) {
-								int len = 0;
-								tmp_str = strrchr(alias, '[');	
-								if(tmp_str) {
-									len = tmp_str - alias;
-								}
-								tmp_str = gdml_zmalloc(len + 1);
-								memcpy(tmp_str, alias, len);
-								tmp_str[len] = '\0';	
-								alias = tmp_str;
-							}
-					} else {
-							alias = get_symbol_alias(sym);
-					}
-					D("%s", alias);
+				} else {
+					alias = get_symbol_alias(sym);
+				}
+				D("%s", alias);
 			} else {
-					D("%s", name);
+				D("%s", name);
 			}
 			p = p->next;
-	   } else if(ni->index) {
+		} else if(ni->index) {
 			while(!ni->node && ni->index) {
-					D("[");
-					translate(ni->index);
-					D("]");
-					p = p->next;
-					if(p == head) {
-							break;
-					} else {
-							ni = list_entry(p, node_info_t, entry);
-					}
+				D("[");
+				translate(ni->index);
+				D("]");
+				p = p->next;
+				if(p == head) {
+					break;
+				} else {
+					ni = list_entry(p, node_info_t, entry);
+				}
 			}
 			node = ni->node;
 			if(p != head) {
-					goto normal_case;
+				goto normal_case;
 			}
 		}
 	}
@@ -274,7 +274,7 @@ static object_t* get_bank_obj(symtab_t table, const char* name) {
 	if (head_ptr != childs_ptr) {
 		obj = (object_t*)(list_entry(head, object_t, entry));
 	} else {
-	/* when a device not have bank, the default_bank is template bank*/
+		/* when a device not have bank, the default_bank is template bank*/
 		obj = gdml_zmalloc(sizeof(object_t));
 		sym = get_symbol_from_root_table("bank", TEMPLATE_TYPE);
 		obj->name = strdup(name);
@@ -297,30 +297,30 @@ static object_t* get_bank_obj(symtab_t table, const char* name) {
 static object_t* get_parameter_obj(symtab_t table, const char* name) {
 	assert(table != NULL); assert(name != NULL);
 	symbol_t symbol = symbol_find(table, name, PARAMETER_TYPE);
-        parameter_attr_t* attr = symbol->attr;
-        //if (attr->is_original == 0) return;
-        if (attr->is_original == 0) return NULL;
-        tree_t* node = attr->common.node;
-        tree_t* param_spec = node->param.paramspec;
-        tree_t* expr_node = NULL;
-        if (param_spec && param_spec->paramspec.expr) {
-                expr_node = param_spec->paramspec.expr;
-        } else {
-                return NULL;
-        }
-        tree_t* ident = NULL;
-        symbol_t obj_sym = NULL;
-        if (expr_node->common.type == QUOTE_TYPE) {
-                ident = expr_node->quote.ident;
-                obj_sym = symbol_find(table, ident->ident.str, OBJECT_TYPE);
-                if (obj_sym) {
-                        return (object_t*)(obj_sym->attr);
-                }
-        }
-        else {
+	parameter_attr_t* attr = symbol->attr;
+	//if (attr->is_original == 0) return;
+	if (attr->is_original == 0) return NULL;
+	tree_t* node = attr->common.node;
+	tree_t* param_spec = node->param.paramspec;
+	tree_t* expr_node = NULL;
+	if (param_spec && param_spec->paramspec.expr) {
+		expr_node = param_spec->paramspec.expr;
+	} else {
 		return NULL;
-        }
-        return (object_t*)(obj_sym->attr);
+	}
+	tree_t* ident = NULL;
+	symbol_t obj_sym = NULL;
+	if (expr_node->common.type == QUOTE_TYPE) {
+		ident = expr_node->quote.ident;
+		obj_sym = symbol_find(table, ident->ident.str, OBJECT_TYPE);
+		if (obj_sym) {
+			return (object_t*)(obj_sym->attr);
+		}
+	}
+	else {
+		return NULL;
+	}
+	return (object_t*)(obj_sym->attr);
 }
 
 /**
@@ -414,112 +414,112 @@ static object_t* get_other_type_obj(symtab_t table, const char* parent_name, con
  * @return : symbol of the last reference of expression
  */
 symbol_t get_ref_sym(tree_t *t, ref_ret_t *ret, symtab_t table){
-		ref_info_t *fi;
-        tree_t *node;
-        tree_t *tmp;
-        tree_t *node_next;
-        tree_t *node3;
-        node_info_t *ni_next;
-        node_info_t *ni;
-        struct list_head *p;
-        symbol_t sym = NULL;
-		const char* fore_name = NULL;
-        const char *name = NULL;
-        const char *name2 = NULL;
-        struct symtab *symtab;
-        int ref_obj = 0;
-        object_t *obj = NULL;
-        object_t *obj2 = NULL;
-		int has_interface = 0;
-		tree_t *index = NULL;
-	
-		if(!table) {
-			symtab = current_table;
-		} else {
-			symtab = table;
-		}
-        /*collect ref info*/
-		fi = new_ref_info();
-		collect_ref_info(t, fi);
-		ret->ref = fi;
-		//ref_info_print(fi);
-		p = fi->list.next;
-        if(p){
-                ni = list_entry(p, node_info_t, entry);
-                node = ni->node;
-		//printf("node name: %s\n", node->common.name);
-                if(node->common.type == IDENT_TYPE){
-                        ref_obj = 0;
-                        name = node->ident.str;
-                        if(is_simics_api(name)) {
-                                ret->is_obj = 0;
-                                goto end;
-                        }
-                }else if(node->common.type == QUOTE_TYPE){
-                        ref_obj = 1;
-                        node = node->quote.ident;
-                        name = node->ident.str;
-                }else if(node->common.type == DML_KEYWORD_TYPE){
-                        ref_obj = 1;
-                        name = node->ident.str;
-                } else {
-                        my_DBG("error component foramt, type %d\n", node->common.type);
-                }
-        }
-        sym = symbol_find_notype(symtab, name);
-        if(!ref_obj && sym->type != OBJECT_TYPE){/*just printf it*/
-                ret->is_obj = 0;
-                goto end;
-        }
-        ret->is_obj = 1;
-        /*maybe check expression*/
-        p = fi->list.next;
-        struct list_head *head = &fi->list;
-        my_DBG("outof\n");
-		if(p->next == p) {
-                /*only one node*/
-                goto end;
-        }
+	ref_info_t *fi;
+	tree_t *node;
+	tree_t *tmp;
+	tree_t *node_next;
+	tree_t *node3;
+	node_info_t *ni_next;
+	node_info_t *ni;
+	struct list_head *p;
+	symbol_t sym = NULL;
+	const char* fore_name = NULL;
+	const char *name = NULL;
+	const char *name2 = NULL;
+	struct symtab *symtab;
+	int ref_obj = 0;
+	object_t *obj = NULL;
+	object_t *obj2 = NULL;
+	int has_interface = 0;
+	tree_t *index = NULL;
 
-        while(p->next != head)
-        {
-				ni = list_entry(p, node_info_t, entry);
-				ni_next = list_entry(p->next, node_info_t, entry);
-				node = ni->node;
-                node_next = ni_next->node;
-normal_case:
-                if(node_next) {
-                        if(node_next->common.type == DML_KEYWORD_TYPE) {
-                                if(node->common.type == QUOTE_TYPE){
-                                        tmp = node->quote.ident;
-                                        name = tmp->ident.str;
-                                } else if(node->common.type == IDENT_TYPE || node->common.type == DML_KEYWORD_TYPE){
-                                        name = node->ident.str;
-                                } else {
-                                        my_DBG("error format\n");
-                                }
-                                node3 = ((node_info_t *)ni_next->entry.next)->node;
-                                if(node3->common.type == IDENT_TYPE){
-                                        name2 = node3->ident.str;
-                                } else if (node3->common.type == DML_KEYWORD_TYPE){
-                                        name2 = node3->ident.str;
+	if(!table) {
+		symtab = current_table;
+	} else {
+		symtab = table;
+	}
+	/*collect ref info*/
+	fi = new_ref_info();
+	collect_ref_info(t, fi);
+	ret->ref = fi;
+	//ref_info_print(fi);
+	p = fi->list.next;
+	if(p){
+		ni = list_entry(p, node_info_t, entry);
+		node = ni->node;
+		//printf("node name: %s\n", node->common.name);
+		if(node->common.type == IDENT_TYPE){
+			ref_obj = 0;
+			name = node->ident.str;
+			if(is_simics_api(name)) {
+				ret->is_obj = 0;
+				goto end;
+			}
+		}else if(node->common.type == QUOTE_TYPE){
+			ref_obj = 1;
+			node = node->quote.ident;
+			name = node->ident.str;
+		}else if(node->common.type == DML_KEYWORD_TYPE){
+			ref_obj = 1;
+			name = node->ident.str;
+		} else {
+			my_DBG("error component foramt, type %d\n", node->common.type);
+		}
+	}
+	sym = symbol_find_notype(symtab, name);
+	if(!ref_obj && sym->type != OBJECT_TYPE){/*just printf it*/
+		ret->is_obj = 0;
+		goto end;
+	}
+	ret->is_obj = 1;
+	/*maybe check expression*/
+	p = fi->list.next;
+	struct list_head *head = &fi->list;
+	my_DBG("outof\n");
+	if(p->next == p) {
+		/*only one node*/
+		goto end;
+	}
+
+	while(p->next != head)
+	{
+		ni = list_entry(p, node_info_t, entry);
+		ni_next = list_entry(p->next, node_info_t, entry);
+		node = ni->node;
+		node_next = ni_next->node;
+	normal_case:
+		if(node_next) {
+			if(node_next->common.type == DML_KEYWORD_TYPE) {
+				if(node->common.type == QUOTE_TYPE){
+					tmp = node->quote.ident;
+					name = tmp->ident.str;
+				} else if(node->common.type == IDENT_TYPE || node->common.type == DML_KEYWORD_TYPE){
+					name = node->ident.str;
 				} else {
-                                        my_DBG("only ident valid\n");
-                                }
-                                if(!name2 || !name){
-                                        my_DBG("error name null\n");
-                                }
+					my_DBG("error format\n");
+				}
+				node3 = ((node_info_t *)ni_next->entry.next)->node;
+				if(node3->common.type == IDENT_TYPE){
+					name2 = node3->ident.str;
+				} else if (node3->common.type == DML_KEYWORD_TYPE){
+					name2 = node3->ident.str;
+				} else {
+					my_DBG("only ident valid\n");
+				}
+				if(!name2 || !name){
+					my_DBG("error name null\n");
+				}
 				sym = symbol_find(symtab, name, OBJECT_TYPE);
 				if (sym) {
 					obj = (object_t*)(sym->attr);
 				} else {
 					obj = get_other_type_obj(symtab, fore_name, name);
 				}
-                                symtab = obj->symtab;
+				symtab = obj->symtab;
 				if (symtab->is_parsed == 0 && obj->node) {
 					parse_unparsed_obj(obj->node, symtab);
 				}
-                                my_DBG("object name %s, name %s\n", obj->name, name2);
+				my_DBG("object name %s, name %s\n", obj->name, name2);
 				if(!strcmp(obj->obj_type, "interface")) {
 					obj2 = obj;
 					ret->con = NULL;
@@ -531,51 +531,51 @@ normal_case:
 					break;
 				}
 				symtab->obj = obj;
-                                sym = symbol_find_notype(symtab, name2);
-                                if(sym){
+				sym = symbol_find_notype(symtab, name2);
+				if(sym){
 					my_DBG("sym type %d, interface type %d\n", sym->type, INTERFACE_TYPE);
-                                        /*may be interface function need */
-                                        if(sym->type == OBJECT_TYPE) {
-                                                obj2 = (object_t *)sym->attr;
-                                                if(!strcmp(obj2->obj_type, "interface")) {
-                                                        my_DBG("interface object\n");
-                                                        ret->con = obj;
-                                                        ret->iface = obj2;
-                                                        node = t->component.ident;
-                                                        name = node->ident.str;
-                                                        ret->method = name;
+					/*may be interface function need */
+					if(sym->type == OBJECT_TYPE) {
+						obj2 = (object_t *)sym->attr;
+						if(!strcmp(obj2->obj_type, "interface")) {
+							my_DBG("interface object\n");
+							ret->con = obj;
+							ret->iface = obj2;
+							node = t->component.ident;
+							name = node->ident.str;
+							ret->method = name;
 							ret->index = index;
 							has_interface = 1;
-                                                        break;
-                                                }
-                                        }
-                                }
+							break;
+						}
+					}
+				}
 				else {
 					printf("symbol '%s' not find\n", name2);
 				}
-                        }
-                        p = ni_next->entry.next;
+			}
+			p = ni_next->entry.next;
 			fore_name = name;
-                } else if (ni_next->index) {
-					/*skip index*/
-					while(ni_next->index) {
-							index = ni_next->index;
-							p = ni_next->entry.next;
-							ni_next = list_entry(p, node_info_t, entry);
-					}
-					node_next = ni_next->node;;
-					if(p != head) {
-							goto normal_case;
-					}
-				}
-        }
+		} else if (ni_next->index) {
+			/*skip index*/
+			while(ni_next->index) {
+				index = ni_next->index;
+				p = ni_next->entry.next;
+				ni_next = list_entry(p, node_info_t, entry);
+			}
+			node_next = ni_next->node;;
+			if(p != head) {
+				goto normal_case;
+			}
+		}
+	}
 	ret->index = index;
 	if(has_interface && ret->con) {
-		 /*just ref object part*/
-		 p = &ni_next->entry;
-		 p->prev->next = head;
+		/*just ref object part*/
+		p = &ni_next->entry;
+		p->prev->next = head;
 	}
 end:
-        my_DBG("out of 2\n");
-        return sym;
+	my_DBG("out of 2\n");
+	return sym;
 }
