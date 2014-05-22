f25ab219683080ed133b03833b3d82c62f57a44f
alloc <alloc.young@gmail.com>
2014-05-22 10:17:20 +0800

    Add more print info
    Add more print info
    
    Signed-off-by: shenoubang <shenoubang@gmail.com>

--- decl.c
+++ decl.c
@@ -88,6 +88,7 @@ void parse_data_cdecl(tree_t* node, symtab_t table) {
 	decl->node = node;
 	node->common.cdecl = decl;
 	symbol_insert(table, decl->var_name, DATA_TYPE, decl);
+	fprintf(stderr, "insert date type, name %s\n", decl->var_name);
 	return;
 }
 
@@ -1235,8 +1236,11 @@ void parse_typedef_cdecl(tree_t* node, symtab_t table) {
 	else {
 		if ((type->var_name) && (type->common.no_decalare == 0)) {
 			type->typedef_name = strdup(type->var_name);
-			if (symbol_insert(table, type->var_name, TYPEDEF_T, type))
+			if (symbol_insert(table, type->var_name, TYPEDEF_T, type)) {
+				int *p = NULL;
+				*p = 0;
 				error("duplicate defined '%s'\n", type->var_name);
+			}
 		}
 	}
 
