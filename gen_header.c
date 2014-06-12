26a787e49dedb8d11125dc5069d54defc4a4926f
alloc <alloc.young@gmail.com>
2014-06-12 15:54:21 +0800

    Add footer
    Add footer
    
    Signed-off-by: shenoubang <shenoubang@gmail.com>

--- gen_header.c
+++ gen_header.c
@@ -21,6 +21,30 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #include "gen_header.h"
+static LIST_HEAD(headers); 
+static LIST_HEAD(footers);
+typedef struct {
+	struct list_head entry;
+	tree_t *node;
+} nodex_t;
+
+
+static void add_to_list(struct list_head *list, tree_t *node) {
+	nodex_t *e;
+	
+	e = (nodex_t *)gdml_zmalloc(sizeof *e);
+	INIT_LIST_HEAD(&e->entry);	
+	e->node = node;	
+	list_add_tail(&e->entry, list);
+}
+
+void add_header(tree_t *node) {
+	add_to_list(&headers, node);
+}
+
+void add_footer(tree_t *node) {
+	add_to_list(&footers, node);
+}
 
 int simics_include_dir(const char *dir) {
 	const char *simics_include_str = "#include <simics";	
@@ -46,24 +70,33 @@ static void gen_header(node_entry_t *nodex, FILE *f) {
 	fprintf(f, "%s", node->head.str);
 }
 
+static void gen_header2(nodex_t *nodex, FILE *f) {
+	tree_t *node = nodex->node;
+	int ret = 0;
+
+	ret = simics_include_dir(node->head.str);
+	if(ret)
+		return;
+	out = f;
+	gen_src_loc(&node->common.location);
+	fprintf(f, "%s", node->head.str);
+}
 void gen_device_header(device_t *dev, FILE *f) {
 	struct list_head *p;
-	tree_t *node;
-	node_entry_t *head;
+	nodex_t *head;
 
-	list_for_each(p, &dev->headers) {
-		head = list_entry(p, node_entry_t, entry);
-		gen_header(head, f);
+	list_for_each(p, &headers) {
+		head = list_entry(p, nodex_t, entry);
+		gen_header2(head, f);
 	}
 }
 
 void gen_device_footer(device_t *dev, FILE *f) {
 	struct list_head *p;
-	tree_t *node;
-	node_entry_t *head;
+	nodex_t *head;
 
-	list_for_each(p, &dev->footers) {
-		head = list_entry(p, node_entry_t, entry);
-		gen_header(head, f);
+	list_for_each(p, &footers) {
+		head = list_entry(p, nodex_t, entry);
+		gen_header2(head, f);
 	}
 }
