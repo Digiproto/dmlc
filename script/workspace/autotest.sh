240a07c654600e277572f5a40868f33214095f18
shenoubang <shenoubang@gmail.com>
2014-06-27 17:56:22 +0800

    add the script the make test and clean
    add the script the make test and clean all the test case
    
    Signed-off-by: shenoubang <shenoubang@gmail.com>

---
+++ script/workspace/autotest.sh
@@ -0,0 +1,292 @@
+#!/bin/bash
+
+./bin/workspace create ADM706
+cp ~/test_case/ADM706/ADM706.dml modules/ADM706/
+
+./bin/workspace create cy22393
+cp ~/test_case/cy22393/cy22393.dml modules/cy22393/
+
+./bin/workspace create lm80
+cp ~/test_case/lm80/*.dml modules/lm80/
+
+./bin/workspace create my_dma
+cp ~/test_case/my_dma/*.dml modules/my_dma/
+
+./bin/workspace create st16c2552
+cp ~/test_case/st16c2552/*.dml modules/st16c2552/
+
+./bin/workspace create tci6482-vcp2
+cp ~/test_case/tci6482-vcp2/*.dml modules/tci6482-vcp2/
+
+./bin/workspace create wbbi_c64xplus_megamodule
+cp ~/test_case/wbbi_c64xplus_megamodule/*.dml modules/wbbi_c64xplus_megamodule/
+
+./bin/workspace create xsi-intr
+cp ~/test_case/xsi-intr/*.dml modules/xsi-intr/
+
+./bin/workspace create aintc
+cp ~/test_case/aintc/*.dml modules/aintc/
+
+./bin/workspace create DEC21140A-dml
+cp ~/test_case/DEC21140A-dml/*.dml modules/DEC21140A-dml/
+
+./bin/workspace create mpc8260_adm706
+cp ~/test_case/mpc8260_adm706/*.dml modules/mpc8260_adm706/
+
+./bin/workspace create pef22554
+cp ~/test_case/pef22554/*.dml modules/pef22554/
+
+./bin/workspace create sd6182-timer
+cp ~/test_case/sd6182-timer/*.dml modules/sd6182-timer/
+
+./bin/workspace create sta101
+cp ~/test_case/sta101/*.dml modules/sta101/
+
+./bin/workspace create ti-semaphore
+cp ~/test_case/ti-semaphore/*.dml modules/ti-semaphore/
+
+./bin/workspace create wbbi_c64xplus_pllctl
+cp ~/test_case/wbbi_c64xplus_pllctl/*.dml modules/wbbi_c64xplus_pllctl/
+
+./bin/workspace create xsi-npe
+cp ~/test_case/xsi-npe/*.dml modules/xsi-npe/
+
+./bin/workspace create arm_i2c
+cp ~/test_case/arm_i2c/*.dml modules/arm_i2c/
+
+./bin/workspace create eeprom
+cp ~/test_case/eeprom/*.dml modules/eeprom/
+
+./bin/workspace create mpc8323-qe-cpri-mac
+cp ~/test_case/mpc8323-qe-cpri-mac/*.dml modules/mpc8323-qe-cpri-mac/
+
+./bin/workspace create pi7c9x110
+cp ~/test_case/pi7c9x110/*.dml modules/pi7c9x110/
+
+./bin/workspace create sd6182-uart
+cp ~/test_case/sd6182-uart/*.dml modules/sd6182-uart/
+
+./bin/workspace create STW81102
+cp ~/test_case/STW81102/*.dml modules/STW81102/
+
+./bin/workspace create TLV5638
+cp ~/test_case/TLV5638/*.dml modules/TLV5638/
+
+./bin/workspace create wbbi_dsp_intc
+cp ~/test_case/wbbi_dsp_intc/*.dml modules/wbbi_dsp_intc/
+
+./bin/workspace create xsi-pci
+cp ~/test_case/xsi-pci/*.dml modules/xsi-pci/
+
+./bin/workspace create arm-mac
+cp ~/test_case/arm-mac/*.dml modules/arm-mac/
+
+./bin/workspace create flash-hook
+cp ~/test_case/flash-hook/*.dml modules/flash-hook/
+
+./bin/workspace create mpc8323-qe-hdlc
+cp ~/test_case/mpc8323-qe-hdlc/*.dml modules/mpc8323-qe-hdlc/
+
+./bin/workspace create pm7340
+cp ~/test_case/pm7340/*.dml modules/pm7340/
+
+./bin/workspace create sdram_spd
+cp ~/test_case/sdram_spd/*.dml modules/sdram_spd/
+
+./bin/workspace create tci6482-edma3
+cp ~/test_case/tci6482-edma3/*.dml modules/tci6482-edma3/
+
+./bin/workspace create wbbi_arm1136_dummy
+cp ~/test_case/wbbi_arm1136_dummy/*.dml modules/wbbi_arm1136_dummy/
+
+./bin/workspace create wbbi-dsp-tcp2
+cp ~/test_case/wbbi-dsp-tcp2/*.dml modules/wbbi-dsp-tcp2/
+
+./bin/workspace create xsi-timer
+cp ~/test_case/xsi-timer/*.dml modules/xsi-timer/
+
+./bin/workspace create bcm5241
+cp ~/test_case/bcm5241/*.dml modules/bcm5241/
+
+./bin/workspace create gptimer
+cp ~/test_case/gptimer/*.dml modules/gptimer/
+
+./bin/workspace create mpc8323-qe-mux-hook
+cp ~/test_case/mpc8323-qe-mux-hook/*.dml modules/mpc8323-qe-mux-hook/
+
+./bin/workspace create PM8363
+cp ~/test_case/PM8363/*.dml modules/PM8363/
+
+./bin/workspace create SRRU-DSP-CACHE
+cp ~/test_case/SRRU-DSP-CACHE/*.dml modules/SRRU-DSP-CACHE/
+
+./bin/workspace create tci6482-emac
+cp ~/test_case/tci6482-emac/*.dml modules/tci6482-emac/
+
+./bin/workspace create wbbi_arm1136_emif
+cp ~/test_case/wbbi_arm1136_emif/*.dml modules/wbbi_arm1136_emif/
+
+./bin/workspace create wbbi-dsp-timer
+cp ~/test_case/wbbi-dsp-timer/*.dml modules/wbbi-dsp-timer/
+
+./bin/workspace create bcm5389
+cp ~/test_case/bcm5389/*.dml modules/bcm5389/
+
+./bin/workspace create hac_sem
+cp ~/test_case/hac_sem/*.dml modules/hac_sem/
+
+./bin/workspace create mpc8323-qe-tdm
+cp ~/test_case/mpc8323-qe-tdm/*.dml modules/mpc8323-qe-tdm/
+
+./bin/workspace create sd6181-soc-vic
+cp ~/test_case/sd6181-soc-vic/*.dml modules/sd6181-soc-vic/
+
+./bin/workspace create SRRU_DSP_IDMA
+cp ~/test_case/SRRU_DSP_IDMA/*.dml modules/SRRU_DSP_IDMA/
+
+./bin/workspace create tci6482-i2c
+cp ~/test_case/tci6482-i2c/*.dml modules/tci6482-i2c/
+
+./bin/workspace create wbbi_arm1136_gpio
+cp ~/test_case/wbbi_arm1136_gpio/*.dml modules/wbbi_arm1136_gpio/
+
+./bin/workspace create wbbi-dsp-vcp2
+cp ~/test_case/wbbi-dsp-vcp2/*.dml modules/wbbi-dsp-vcp2/
+
+./bin/workspace create bcm5461
+cp ~/test_case/bcm5461/*.dml modules/bcm5461/
+
+./bin/workspace create hbbu-sfp
+cp ~/test_case/hbbu-sfp/*.dml modules/hbbu-sfp/
+
+./bin/workspace create MPC8323-QE-TIMER
+cp ~/test_case/MPC8323-QE-TIMER/*.dml modules/MPC8323-QE-TIMER/
+
+./bin/workspace create sd6182-gic
+cp ~/test_case/sd6182-gic/*.dml modules/sd6182-gic/
+
+./bin/workspace create SRRU_DSP_INTC
+cp ~/test_case/SRRU_DSP_INTC/*.dml modules/SRRU_DSP_INTC/
+
+./bin/workspace create tci6482-intc
+cp ~/test_case/tci6482-intc/*.dml modules/tci6482-intc/
+
+./bin/workspace create wbbi_arm1136_intc
+cp ~/test_case/wbbi_arm1136_intc/*.dml modules/wbbi_arm1136_intc/
+
+./bin/workspace create wbbi-gpio-timer
+cp ~/test_case/wbbi-gpio-timer/*.dml modules/wbbi-gpio-timer/
+
+./bin/workspace create hidsp-icu
+cp ~/test_case/hidsp-icu/*.dml modules/hidsp-icu/
+
+./bin/workspace create mpc8323-qe-ucc-hook
+cp ~/test_case/mpc8323-qe-ucc-hook/*.dml modules/mpc8323-qe-ucc-hook/
+
+./bin/workspace create sd6182-gmac
+cp ~/test_case/sd6182-gmac/*.dml modules/sd6182-gmac/
+
+./bin/workspace create SRRU-DSP-TIMER
+cp ~/test_case/SRRU-DSP-TIMER/*.dml modules/SRRU-DSP-TIMER/
+
+./bin/workspace create tci6482-loader
+cp ~/test_case/tci6482-loader/*.dml modules/tci6482-loader/
+
+./bin/workspace create wbbi-arm1136-reset
+cp ~/test_case/wbbi-arm1136-reset/*.dml modules/wbbi-arm1136-reset/
+
+./bin/workspace create wbbi_ucc_hook
+cp ~/test_case/wbbi_ucc_hook/*.dml modules/wbbi_ucc_hook/
+
+./bin/workspace create BCM5646
+cp ~/test_case/BCM5646/*.dml modules/BCM5646/
+
+./bin/workspace create hidsp-timer
+cp ~/test_case/hidsp-timer/*.dml modules/hidsp-timer/
+
+./bin/workspace create mpc8541-cpm-cp
+cp ~/test_case/mpc8541-cpm-cp/*.dml modules/mpc8541-cpm-cp/
+
+./bin/workspace create sd6182-gmsgm
+cp ~/test_case/sd6182-gmsgm/*.dml modules/sd6182-gmsgm/
+
+./bin/workspace create SRRU-EMIF
+cp ~/test_case/SRRU-EMIF/*.dml modules/SRRU-EMIF/
+
+./bin/workspace create tci6482-megamodule
+cp ~/test_case/tci6482-megamodule/*.dml modules/tci6482-megamodule/
+
+./bin/workspace create wbbi_arm1136_sysctl
+cp ~/test_case/wbbi_arm1136_sysctl/*.dml modules/wbbi_arm1136_sysctl/
+
+./bin/workspace create xsi-csr
+cp ~/test_case/xsi-csr/*.dml modules/xsi-csr/
+
+./bin/workspace create HiSEMV230
+cp ~/test_case/HiSEMV230/*.dml modules/HiSEMV230/
+
+./bin/workspace create mpc8541-cpm-fcc
+cp ~/test_case/mpc8541-cpm-fcc/*.dml modules/mpc8541-cpm-fcc/
+
+./bin/workspace create sd6182-gpio
+cp ~/test_case/sd6182-gpio/*.dml modules/sd6182-gpio/
+
+./bin/workspace create SRRU_GPIO
+cp ~/test_case/SRRU_GPIO/*.dml modules/SRRU_GPIO/
+
+./bin/workspace create tci6482-pllctl
+cp ~/test_case/tci6482-pllctl/*.dml modules/tci6482-pllctl/
+
+./bin/workspace create wbbi_arm1136_timer
+cp ~/test_case/wbbi_arm1136_timer/*.dml modules/wbbi_arm1136_timer/
+
+./bin/workspace create xsi-emac
+cp ~/test_case/xsi-emac/*.dml modules/xsi-emac/
+
+./bin/workspace create CDCM7005
+cp ~/test_case/CDCM7005/*.dml modules/CDCM7005/
+
+./bin/workspace create ISP1160
+cp ~/test_case/ISP1160/*.dml modules/ISP1160/
+
+./bin/workspace create mpc8541-cpm-mux
+cp ~/test_case/mpc8541-cpm-mux/*.dml modules/mpc8541-cpm-mux/
+
+./bin/workspace create sd6182-mss
+cp ~/test_case/sd6182-mss/*.dml modules/sd6182-mss/
+
+./bin/workspace create SRRU-SPI-CONTROLLER
+cp ~/test_case/SRRU-SPI-CONTROLLER/*.dml modules/SRRU-SPI-CONTROLLER/
+
+./bin/workspace create tci6482-tcp2
+cp ~/test_case/tci6482-tcp2/*.dml modules/tci6482-tcp2/
+
+./bin/workspace create wbbi_c64xplus_dummy
+cp ~/test_case/wbbi_c64xplus_dummy/*.dml modules/wbbi_c64xplus_dummy/
+
+./bin/workspace create xsi-gmac
+cp ~/test_case/xsi-gmac/*.dml modules/xsi-gmac/
+
+./bin/workspace create cfcard
+cp ~/test_case/cfcard/*.dml modules/cfcard/
+
+./bin/workspace create lm75
+cp ~/test_case/lm75/*.dml modules/lm75/
+
+./bin/workspace create mpc8541-cpm-pic
+cp ~/test_case/mpc8541-cpm-pic/*.dml modules/mpc8541-cpm-pic/
+
+./bin/workspace create sd6182-sem
+cp ~/test_case/sd6182-sem/*.dml modules/sd6182-sem/
+
+./bin/workspace create SRRU_SYS_CTL
+cp ~/test_case/SRRU_SYS_CTL/*.dml modules/SRRU_SYS_CTL/
+
+./bin/workspace create tci6482-timer
+cp ~/test_case/tci6482-timer/*.dml modules/tci6482-timer/
+
+./bin/workspace create wbbi_c64xplus_emac
+cp ~/test_case/wbbi_c64xplus_emac/*.dml modules/wbbi_c64xplus_emac/
+
+./bin/workspace create xsi-gpio
+cp ~/test_case/xsi-gpio/*.dml modules/xsi-gpio/
